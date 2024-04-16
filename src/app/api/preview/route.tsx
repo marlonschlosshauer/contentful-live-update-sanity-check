6d7361e7fd08706d9566c02b25b08e0b6c8d9408
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2024-04-16 17:31:22 +0200

    Apply contentful draftmode bypass trick

--- src/app/api/preview/route.tsx
+++ src/app/api/preview/route.tsx
@@ -1,7 +1,21 @@
-import { draftMode } from "next/headers";
+import { cookies, draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function GET() {
   draftMode().enable();
+
+  // Override cookie header for draft mode for usage in live-preview
+  // https://github.com/vercel/next.js/issues/49927
+  const cookieStore = cookies();
+  const cookie = cookieStore.get("__prerender_bypass")!;
+  cookies().set({
+    name: "__prerender_bypass",
+    value: cookie?.value,
+    httpOnly: true,
+    path: "/",
+    secure: true,
+    sameSite: "none",
+  });
+
   redirect("/preview");
 }
