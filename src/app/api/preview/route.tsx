beba0f543c57c0ac34824002d134520f8932cf03
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2024-04-17 09:58:24 +0200

    Remove cookie override for now

--- src/app/api/preview/route.tsx
+++ src/app/api/preview/route.tsx
@@ -1,21 +1,8 @@
-import { cookies, draftMode } from "next/headers";
+import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function GET() {
+  console.log("Activating draft mode");
   draftMode().enable();
-
-  // Override cookie header for draft mode for usage in live-preview
-  // https://github.com/vercel/next.js/issues/49927
-  const cookieStore = cookies();
-  const cookie = cookieStore.get("__prerender_bypass")!;
-  cookies().set({
-    name: "__prerender_bypass",
-    value: cookie?.value,
-    httpOnly: true,
-    path: "/",
-    secure: true,
-    sameSite: "none",
-  });
-
   redirect("/preview");
 }
